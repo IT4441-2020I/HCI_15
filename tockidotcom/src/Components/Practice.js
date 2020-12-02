a4efb7be05065c4cfe2e202bca7532439505dad4
Ly Trung Kien <lytrungkientq99@gmail.com>
2020-12-02 18:38:29 +0700

    feat/practice

--- tockidotcom/src/Components/Practice.js
+++ tockidotcom/src/Components/Practice.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Row, Col, PageHeader, Button, Card, Input } from 'antd';
+import { Row, Col, PageHeader, Button, Card, Input, Statistic } from 'antd';
 import 'antd/dist/antd.css';
 import { SyncOutlined } from '@ant-design/icons';
 import { Content } from 'antd/lib/layout/layout';
@@ -8,9 +8,34 @@ import imagess from './Capture.PNG';
 
 import './Practice.css';
 
+const { Countdown } = Statistic;
+
+const deadline = Date.now() + 5000 * 60; 
 
 class Practice extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            test: 0,
+        }
+    }
+
+    onStart = () => {
+        
+    }
+
+    onChangeKey = () => {
+        this.setState({ test: 1 })
+    }
+
     render() {
+
+        console.log('render' + this.state.test);
+
+        const listKeyBoard1 = ['S', 'K', 'R', 'N', 'H', '*', 'W', 'J', 'N', 'T'];
+        const listKeyBoard2 = ['T', 'P', 'H', 'N', 'S', 'I', 'Y', 'J', 'G', 'K']
+
         return (
             <div
                 style={{
@@ -26,10 +51,16 @@ class Practice extends Component {
                         title="Luyện tập"
                         subTitle="gõ tốc ký Tiếng Việt"
                         extra={[
-                            <Button key="1" type="primary">
-                                Dừng
+                            <Button key="1" type="primary" onPress={this.onStart}>
+                                Bắt đầu
                             </Button>,
                             <Button key="2" type="primary">
+                                Tạm dừng
+                        </Button>,
+                            <Button key="3" type="primary">
+                                Kết thúc
+                    </Button>,
+                            <Button key="4" type="primary">
                                 <SyncOutlined /> Làm mới
                             </Button>
                         ]}
@@ -47,7 +78,7 @@ class Practice extends Component {
                                     boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
                                     fontSize: 32,
                                 }}>
-                                    00 : 00 : 00
+                                   <Countdown value={deadline} />
                                 </div>
                                 <Card
                                     title="Kết quả"
@@ -64,46 +95,15 @@ class Practice extends Component {
                             </div>
                         </Col>
                         <Col span='18'>
-                            {/* <div style={{
+                            <div style={{
                                 margin: 20,
                                 backgroundColor: 'white',
                                 boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
-                                minHeight: 300,
-                                display: 'flex'
+                                minHeight: 500,
                             }}>
-                                <Col span='9'>
-                                    <Card title="Văn bản" bordered={true}>
-                                        <p>{`Chính lòng yêu nước, chứ không phải lý tưởng cộng sản, 
-                                    là nguồn cảm hứng cho tôi.`}</p>
-                                    </Card>
-                                </Col>
-
-                                <Col span='9'>
-                                    <Card title="Tốc ký" bordered={true}>
-                                        <p>{`-TK SK-S*IK SR-HOG -IEJG PR-SEWNK *-AW -K SK-SUW KH-OWG 
-                                        PH-HSAJ SR-SI T-HSEWG KN-OWG S-HSAN *-AW -K SR-HA KR-HUON 
-                                        K-HSAT H-SUWG SK-O T-OWJ -AGK`}</p>
-
-                                    </Card>
-                                </Col>
-                                <div>
-                                    <img
-                                        // width={200}
-                                        // src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                                        src={imagess}
-                                    />
-                                </div> */}
-                            <Row>
-
-                                <div style={{
-                                    margin: 20,
-                                    backgroundColor: 'white',
-                                    boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
-                                    minHeight: 300,
-                                    display: 'flex'
-                                }}>
+                                <Row>
                                     <Col span='12'>
-                                        <Card title="Văn bản" bordered={true}>
+                                        <Card title="Văn bản" bordered={true} style={{ minHeight: 210 }}>
                                             <p>{`Chính lòng yêu nước, chứ không phải lý tưởng cộng sản, 
                                     là nguồn cảm hứng cho tôi.`}</p>
                                         </Card>
@@ -117,19 +117,52 @@ class Practice extends Component {
 
                                         </Card>
                                     </Col>
-                                </div>
-                                <Input></Input>
-                            </Row>
-                            <Row>
-                                <Input></Input>
-                            </Row>
-                            <Row>
-                                <img
-                                    src={imagess}
-                                />
-                            </Row>
+                                </Row>
+                                <Row style={{
+                                    margin: 20,
+                                    boxShadow: 'rgba(100, 100, 111, 0.2) 0px 4px 15px 0px',
+                                }}>
+                                    <Input size='large' placeholder="Start practice" onChange={this.onChangeKey} />
+                                </Row>
 
-                            {/* </div> */}
+                                <Row>
+                                    <Col span='6'></Col>
+                                    <Col span='12'>
+                                        <div style={{
+                                            height: 30,
+                                            width: 372,
+                                            backgroundColor: '#67757c',
+                                            fontSize: 18,
+                                            display: 'flex',
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            borderRadius: 5,
+                                            marginRight: 8,
+                                            marginBottom: 2,
+
+                                        }}></div>
+
+                                        <div style={{ display: "flex" }}>
+                                            {
+                                                listKeyBoard1.map((item, id) => <KeyBoard key={id} item={item} />)
+                                            }
+                                        </div>
+
+                                        <div style={{ display: "flex" }}>
+                                            {
+                                                listKeyBoard2.map((item, id) => <KeyBoard key={id} item={item} />)
+                                            }
+                                        </div>
+                                        <div style={{ display: "flex", justifyContent: "center", marginTop: 10 }}>
+                                            <KeyBoard item="U" />
+                                            <KeyBoard item="O" />
+                                            <KeyBoard item="E" />
+                                            <KeyBoard item="A" />
+                                        </div>
+                                    </Col>
+                                    <Col span='6'></Col>
+                                </Row>
+                            </div>
                         </Col>
                     </Row>
                 </Content>
@@ -137,6 +170,29 @@ class Practice extends Component {
 
         );
     }
+}
+
+const KeyBoard = (props) => {
+    return (
+        <div
+            style={{
+                height: 50,
+                width: 30,
+                backgroundColor: 'black',
+                color: 'white',
+                fontSize: 18,
+                display: 'flex',
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 5,
+                marginRight: 8,
+                marginBottom: 2,
+
+            }}
+        >
+            {props.item}
+        </div>
+    )
 }
 
 export default Practice;
